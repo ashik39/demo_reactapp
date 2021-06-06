18fbee15cc19164080f274cc244ba4adea7b0103
saravanan-zaigo <asanashik7@gmail.com>
2021-06-06 19:21:55 +0530

    checking for updation

--- src/App.js
+++ src/App.js
@@ -5,9 +5,10 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <h4>Checking for update in others device</h4>
         <img src={logo} className="App-logo" alt="logo" />
         <p>Edited Title! Production Build added</p>
-        <h2>testig NETLIFY for more functionality</h2>
+        <h2>Testig NETLIFY for more functionality</h2>
       </header>
     </div>
   );
